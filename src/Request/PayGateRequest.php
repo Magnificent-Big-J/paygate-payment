022129302a5fa32a542709805759653e30127c63
Joel Mnisi <joel.mnisi@protonmail.com>
2023-06-29 23:55:42 +0200

    Implement request class for paygate and paysubs

--- src/Request/PayGateRequest.php
+++ src/Request/PayGateRequest.php
@@ -2,7 +2,51 @@
 
 namespace rainwaves\PaygatePayment\Request;
 
-class PayGateRequest
+use rainwaves\PaygatePayment\Abstraction\Arrayable;
+use rainwaves\PaygatePayment\Entities\AmountConverterTrait;
+use rainwaves\PaygatePayment\Entities\PayGateProperties;
+
+class PayGateRequest extends Arrayable
 {
+    use AmountConverterTrait, PayGateProperties;
+
+    private string $payGateInitialUrl = 'https://secure.paygate.co.za/payweb3/initiate.trans';
+
+    private string $payGateQueryUrl = 'https://secure.paygate.co.za/payweb3/query.trans';
+
+    public string $reference;
+    public float $amount;
+    public string $currency;
+    public string $returnUrl;
+
+    public string $notifyUrl;
+
+    public string $transactionDate;
+    public string $locale;
+    public string $country;
+    public string $email;
+
+    public static function inputRequest(\stdClass $input): PayGateRequest
+    {
+        $self = new self();
+        $self->payGateId = $input->payGateId;
+        $self->payGateSecret = $input->payGateSecret;
+        $self->reference = $input->reference;
+        $self->currency = $input->currency;
+        $self->returnUrl = $input->returnUrl;
+        $self->transactionDate = $input->transactionDate;
+        $self->locale = $input->locale;
+        $self->amount = $self->amountInCents($input->amount);
+        $self->country = $input->country;
+        $self->notifyUrl = $input->notifyUrl;
+        $self->email = $input->email;
+
+        return $self;
+    }
+
+    public function getPayGateID(): string
+    {
+        return $this->payGateId;
+    }
 
 }
--- src/Request/PaySubsRequest.php
+++ src/Request/PaySubsRequest.php
@@ -2,7 +2,51 @@
 
 namespace rainwaves\PaygatePayment\Request;
 
-class PaySubsRequest
-{
+use rainwaves\PaygatePayment\Abstraction\Arrayable;
+use rainwaves\PaygatePayment\Entities\AmountConverterTrait;
+use rainwaves\PaygatePayment\Entities\PayGateProperties;
 
+class PaySubsRequest  extends Arrayable
+{
+    use AmountConverterTrait, PayGateProperties;
+    private string $payGateFormAction = 'https://www.paygate.co.za/paysubs/process.trans';
+
+    public string $version;
+    public string $reference;
+    public int $amount;
+    public string $currency;
+    public string $returnUrl;
+    public string $transactionDate;
+    public string $subsStartDate;
+    public string $subsEndDate;
+    public string $subsFrequency;
+    public string $processNow;
+    public string $processNowAmount;
+    public string $checksum;
+
+    public static function inputRequest(array $input): PaySubsRequest
+    {
+        $self = new self();
+        $self->payGateId = $input['payGateId'];
+        $self->payGateSecret = $input['payGateSecret'];
+        $self->version = $input['version'];
+        $self->reference = $input['reference'];
+        $self->amount = $self->amountInCents($input['amount']);
+        $self->currency = $input['currency'];
+        $self->returnUrl = $input['returnUrl'];
+        $self->transactionDate = $input['transactionDate'];
+        $self->subsStartDate = $input['subsStartDate'];
+        $self->subsEndDate = $input['subsEndDate'];
+        $self->subsFrequency = $input['subsFrequency'];
+        $self->processNow = $input['processNow'];
+        $self->processNowAmount = $input['processNowAmount'];
+        $self->checksum = $input['checksum'];
+
+        return $self;
+    }
+
+    private function amountInCents(float $amount): int
+    {
+        return intval($amount * 100);
+    }
 }
