cd54ea194bc49ac5bc7e30ca260f5c4590bed94e
Joel Mnisi <joel.mnisi@protonmail.com>
2023-07-03 22:48:01 +0200

    Use the correct url

--- src/Model/PaymentURL.php
+++ src/Model/PaymentURL.php
@@ -5,5 +5,5 @@ namespace rainwaves\PaygatePayment\Model;
 class PaymentURL
 {
     public const PAY_WEB_INITIAL_URL = 'https://secure.paygate.co.za/payweb3/initiate.trans';
-    public const PAY_WEB_QUERY_URL = 'https://secure.paygate.co.za/payweb3/query.trans';
+    public const PAY_WEB_PROCESS_URL = 'https://secure.paygate.co.za/payweb3/process.trans';
 }
