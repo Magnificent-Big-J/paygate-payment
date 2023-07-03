9fa0fd8c871a48543bad833bc81ce1799be0ec0b
Joel Mnisi <joel.mnisi@protonmail.com>
2023-07-03 22:03:37 +0200

    Refactor PayGateInitialRequest logic

--- src/Request/PayGateInitialRequest.php
+++ src/Request/PayGateInitialRequest.php
@@ -4,49 +4,36 @@ namespace rainwaves\PaygatePayment\Request;
 
 use rainwaves\PaygatePayment\Abstraction\Arrayable;
 use rainwaves\PaygatePayment\Entities\AmountConverterTrait;
-use rainwaves\PaygatePayment\Entities\PayGateProperties;
-
 class PayGateInitialRequest extends Arrayable
 {
-    use AmountConverterTrait, PayGateProperties;
+    use AmountConverterTrait;
 
-    private string $payGateInitialUrl = 'https://secure.paygate.co.za/payweb3/initiate.trans';
-
+    public string $paygateId;
     public string $reference;
-    public float $amount;
+    public int $amount;
     public string $currency;
     public string $returnUrl;
-
-    public string $notifyUrl;
-
     public string $transactionDate;
     public string $locale;
     public string $country;
     public string $email;
 
+    public string $notifyUrl;
+
     public static function inputRequest(array $input): PayGateInitialRequest
     {
         $input = (object)$input;
-
         $self = new self();
-        $self->payGateId = $input->payGateId;
-        $self->payGateSecret = $input->payGateSecret;
+        $self->paygateId = $input->payGateId;
         $self->reference = $input->reference;
+        $self->amount = $self->amountInCents($input->amount);
         $self->currency = $input->currency;
         $self->returnUrl = $input->returnUrl;
+        $self->notifyUrl = $input->notifyUrl;
         $self->transactionDate = $input->transactionDate;
         $self->locale = $input->locale;
-        $self->amount = $self->amountInCents($input->amount);
         $self->country = $input->country;
-        $self->notifyUrl = $input->notifyUrl;
         $self->email = $input->email;
-
         return $self;
     }
-
-    public function getPayGateID(): string
-    {
-        return $this->payGateId;
-    }
-
 }
