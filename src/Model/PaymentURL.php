5a97a1a7e19c74ee15451f3ce1042a8760410f63
Joel Mnisi <joel.mnisi@protonmail.com>
2023-07-03 22:03:06 +0200

    Implement PaymentURL

---
+++ src/Model/PaymentURL.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace rainwaves\PaygatePayment\Model;
+
+class PaymentURL
+{
+    public const PAY_WEB_INITIAL_URL = 'https://secure.paygate.co.za/payweb3/initiate.trans';
+    public const PAY_WEB_QUERY_URL = 'https://secure.paygate.co.za/payweb3/query.trans';
+}
