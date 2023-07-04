a3916847a23a32d02c423430e702716ec3c4345c
Joel Mnisi <joel.mnisi@protonmail.com>
2023-07-04 22:39:48 +0200

    Implement pay subs

--- src/PaySubs.php
+++ src/PaySubs.php
@@ -2,7 +2,31 @@
 
 namespace rainwaves\PaygatePayment\PaySubs;
 
+use rainwaves\PaygatePayment\Client\PaySubsClient;
+
 class PaySubs
 {
+    private PaySubsClient $paySubsClient;
+
+    public function __construct(string $payGateId, string $encryption)
+    {
+        $this->paySubsClient = new PaySubsClient($payGateId, $encryption);
+    }
+
+    public function createSubscription(array $input): array
+    {
+        return $this->paySubsClient->createSubscription($input);
+    }
+
+    public function createSubscriptionAndChain(array $input): PaySubsClient
+    {
+        $this->paySubsClient->createSubscriptionAndChain($input);
+        return  $this;
+    }
+
+    public function createForm(): string
+    {
+        return $this->paySubsClient->createForm();
+    }
 
 }
