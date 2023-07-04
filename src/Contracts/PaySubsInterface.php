b962d1106c1d7f3be979479242d9a9225148bff9
Joel Mnisi <joel.mnisi@protonmail.com>
2023-07-04 21:31:54 +0200

    Implement PaySubsInterface  contract

--- src/Contracts/PaySubsInterface.php
+++ src/Contracts/PaySubsInterface.php
@@ -2,7 +2,11 @@
 
 namespace rainwaves\PaygatePayment\Contracts;
 
+use rainwaves\PaygatePayment\Client\PaySubsClient;
+
 interface PaySubsInterface
 {
+    public function createSubscription(array $input): array;
+    public function createSubscriptionAndChain(array $input): PaySubsClient;
 
 }
