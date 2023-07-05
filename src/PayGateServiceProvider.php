f0ed6aee2e8e66e77d6cc9947d1190fc8de8b3b0
Joel Mnisi <joel.mnisi@protonmail.com>
2023-07-05 23:46:31 +0200

    bind class into an interface

--- src/PayGateServiceProvider.php
+++ src/PayGateServiceProvider.php
@@ -3,6 +3,8 @@
 namespace rainwaves\PaygatePayment;
 
 use Illuminate\Support\ServiceProvider;
+use rainwaves\PaygatePayment\Client\PayWebClient;
+use rainwaves\PaygatePayment\Contracts\PaySubsInterface;
 use rainwaves\PaygatePayment\Contracts\PayWebInterface;
 
 class PayGateServiceProvider extends ServiceProvider
@@ -17,6 +19,12 @@ class PayGateServiceProvider extends ServiceProvider
             $payGateId = config('paygate.paygate_id');
             $payGateSecret = config('paygate.paygate_secret');
             return new PayWeb($payGateId, $payGateSecret);
+        });
+
+        $this->app->bind(PaySubsInterface::class, function ($app) {
+            $payGateId = config('paygate.paygate_id');
+            $payGateSecret = config('paygate.paygate_secret');
+            return new PayWebClient($payGateId, $payGateSecret);
         });
     }
 }
