ac7e18ff60423cd0961252a1692d77a1e90d2dbe
Joel Mnisi <joel.mnisi@protonmail.com>
2023-07-05 22:58:15 +0200

    They must extend FormInterface

--- src/Contracts/PayWebInterface.php
+++ src/Contracts/PayWebInterface.php
@@ -5,7 +5,7 @@ namespace rainwaves\PaygatePayment\Contracts;
 use rainwaves\PaygatePayment\Client\PayWebClient;
 use rainwaves\PaygatePayment\Response\InitialResponse;
 
-interface PayWebInterface
+interface PayWebInterface extends FormInterface
 {
     public function initiatePayment(array $inputData): InitialResponse;
     public function initiatePaymentAndChain(array $inputData): PayWebClient;
